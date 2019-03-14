eaf174c413027e47d487fab0d03f2f5cd3b39eda
John Mitchell <threecar@gmail.com>
2019-03-13 22:43:47 -0400

    v12

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ var app = require('./server/server');
 var logger = require('./server/util/logger');
 
 app.listen(config.port);
-logger.log('listening on http://localhost:' + config.port);
+logger.log('listening on port: ' + config.port);
 
 
