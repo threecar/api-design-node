b7ee16942cd05b4535370ac39157924ccd92b8b9
John Mitchell <threecar@gmail.com>
2019-03-14 21:08:46 -0400

    v3

--- server/config/production.js
+++ server/config/production.js
@@ -1,7 +1,7 @@
 module.exports = {
   // enabled logging for development
   logging: true,
-  seed: true,
+  seed: false,
   db: {
     //  url: 'mongodb://127.0.0.1/nodeblog'
   //url: 'mongodb+srv://backend:NyWy223ZaATFEvZ@cpcluster-tsylb.gcp.mongodb.net/test?retryWrites=true'
